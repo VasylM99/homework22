585cbbcdfc09a3dffc425c4e4236aee6528db5b2
Vasyl <muran.wasyl@ukr.net>
2020-04-29 13:42:25 +0300

    Updates

---
+++ precache-manifest.8c511c3743273193f893d1128ed83c23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4fd84a7e93d59df5ad61a94b8789d4b9",
+    "url": "/homework22/index.html"
+  },
+  {
+    "revision": "00fbe2e6dfd2ed59b650",
+    "url": "/homework22/static/css/main.d2310793.chunk.css"
+  },
+  {
+    "revision": "24e4c2cef1aa6e4e1b3c",
+    "url": "/homework22/static/js/2.eed1f87e.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/homework22/static/js/2.eed1f87e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00fbe2e6dfd2ed59b650",
+    "url": "/homework22/static/js/main.be89bd6a.chunk.js"
+  },
+  {
+    "revision": "625a7c754a99686f8573",
+    "url": "/homework22/static/js/runtime-main.5fc181c5.js"
+  },
+  {
+    "revision": "ca6c1c20400fab63a459c9210ab72de0",
+    "url": "/homework22/static/media/D.ca6c1c20.mp3"
+  },
+  {
+    "revision": "90679e0cfe8c59c45c061a82c48f01d4",
+    "url": "/homework22/static/media/Roboto-Regular.90679e0c.ttf"
+  }
+]);
